b9865d32ae7dc2cdbb1c32684e620686c1485527
Md. Rakib Uddin <r.rony9966@gmail.com>
2022-05-14 16:51:04 +0600

    edit some features

--- resources/views/pages/layouts/footer.blade.php
+++ resources/views/pages/layouts/footer.blade.php
@@ -22,24 +22,24 @@
 
 
 <!-- Bootstrap core JavaScript-->
-<script src="{{ asset('src/vendor/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('src/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ url('src/vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ url('src/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
 <!-- Core plugin JavaScript-->
-<script src="{{ asset('src/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+<script src="{{ url('src/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="{{ asset('src/js/sb-admin-2.min.js') }}"></script>
+<script src="{{ url('src/js/sb-admin-2.min.js') }}"></script>
 
 <!-- Page level plugins -->
-<script src="{{ asset('src/vendor/chart.js/Chart.min.js') }}"></script>
-<script src="{{ asset('src/vendor/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('src/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ url('src/vendor/chart.js/Chart.min.js') }}"></script>
+<script src="{{ url('src/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ url('src/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
 
 <!-- Page level custom scripts -->
-<script src="{{ asset('src/js/demo/chart-area-demo.js') }}"></script>
-<script src="{{ asset('src/js/demo/chart-pie-demo.js') }}"></script>
-<script src="{{ asset('src/js/demo/datatables-demo.js') }}"></script>
+<script src="{{ url('src/js/demo/chart-area-demo.js') }}"></script>
+<script src="{{ url('src/js/demo/chart-pie-demo.js') }}"></script>
+<script src="{{ url('src/js/demo/datatables-demo.js') }}"></script>
 <script src="https://kit.fontawesome.com/88197b63d0.js" crossorigin="anonymous"></script>
 <script>
     function copyToClipboard(text) {
