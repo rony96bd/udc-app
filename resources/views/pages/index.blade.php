9296a75fb3e806bc78b25c88a20bff6d3395efc8
Md. Rakib Uddin <r.rony9966@gmail.com>
2022-05-18 02:51:55 +0600

    Add payment features

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,46 +5,46 @@
     <div class="container-fluid">
 
         <!-- Page Heading -->
-{{--        <div class="d-sm-flex align-items-center justify-content-between mb-6"></div>--}}
+        {{--        <div class="d-sm-flex align-items-center justify-content-between mb-6"></div>--}}
 
         <!-- Content Row -->
         <div class="row">
             <!-- ID Send Card -->
             @if ($user->is_admin == '0')
-            <div class="col-xl-12 col-md-6 mb-4">
+                <div class="col-xl-12 col-md-6 mb-4">
 
-                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card border-left-primary shadow h-100 py-2">
 
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div
-                                    class="text-xs text-center font-weight-bold text-primary text-uppercase mb-1">
-                                    Send ID's
-                                </div>
-                                <div class="text-center h5 mb-0 font-weight-bold text-gray-800">
-                                    <form action="{{ Route('addBr') }}" method="POST"
-                                          class="d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                                        @csrf
-                                        <div class="input-group">
-                                            <input name="brid" type="text" autofocus="on"
-                                                   class="form-control bg-light border-0 small"
-                                                   placeholder="Write ID"
-                                                   aria-label="Send ID" aria-describedby="basic-addon2">
-                                            <div class="input-group-append">
-                                                <button class="btn btn-primary" type="submit">
-                                                    <i class="fas fa-paper-plane fa-sm"></i>
-                                                </button>
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div
+                                        class="text-xs text-center font-weight-bold text-primary text-uppercase mb-1">
+                                        Send ID's
+                                    </div>
+                                    <div class="text-center h5 mb-0 font-weight-bold text-gray-800">
+                                        <form action="{{ Route('addBr') }}" method="POST"
+                                              class="d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                                            @csrf
+                                            <div class="input-group">
+                                                <input name="brid" type="text" autofocus="on"
+                                                       class="form-control bg-light border-0 small"
+                                                       placeholder="Write ID"
+                                                       aria-label="Send ID" aria-describedby="basic-addon2">
+                                                <div class="input-group-append">
+                                                    <button class="btn btn-primary" type="submit">
+                                                        <i class="fas fa-paper-plane fa-sm"></i>
+                                                    </button>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </form>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-            </div>
+                </div>
             @endif
         </div>
 
@@ -154,20 +154,15 @@
                                                       placeholder="{{ $brId->message }}">{{ $brId->message }}</textarea>
                                         @endif
                                     </td>
-
                                     @if ($user->is_admin == '1')
                                         <td style="vertical-align: middle;">
                                             {{--                                            <span>{{ $brId->name }}</span><br>--}}
                                             <span>{{ $brId->email }}</span>
                                         </td>
-                                        <td style="vertical-align: middle;">
-                                            <div class="d-flex justify-content-between">
+                                        <td class="d-flex justify-content-between" style="vertical-align: middle;">
                                                 <span> <button type="submit" class="btn btn-primary btn-sm"><i
                                                             class="fas fa-save"></i></button> </span>
-
-
                                 </form>
-
                                 <form action="{{ route('deleteBr', $brId->id) }}" method="post">
                                     @csrf
                                     @method('DELETE')
@@ -175,16 +170,15 @@
                                                 class="fas fa-trash"></i>
                                         </button></span>
                                 </form>
-                            </div>
-                                    </td>
-                               @endif
-                                    </tr>
-                               @endforeach
-                            </tbody>
+                                </td>
+                                @endif
+                            </tr>
+                        @endforeach
+                        </tbody>
                     </table>
+                </div>
             </div>
         </div>
-    </div>
     </div>
     <!-- /.container-fluid -->
     </div>
--- resources/views/pages/payinfo.blade.php
+++ resources/views/pages/payinfo.blade.php
@@ -1,61 +1,74 @@
 @extends('pages.layouts.main')
 @section('main-container')
-                <!-- Begin Page Content -->
-            <div class="container-fluid">
-                <!-- DataTales For Payment -->
-
-                    <!-- DataTales For Payment -->
-                    <div class="card shadow mb-4">
-                        <div class="card-header py-3">
-                            <h4 style="float: left" class="m-0 font-weight-bold text-primary">Payments</h4>
-                            <a href="{{ URL::to('/add-payment') }}"><button type="button" class="btn btn-success float-right"><i class="fas fa-plus-circle pt-1"></i>  Add Payment</button></a>
-                        </div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                    <thead>
-                                    <tr>
-                                        <th>Date</th>
-                                        <th>Paid By</th>
-                                        <th>Taka</th>
-                                        <th>Transaction Id</th>
-                                        <th>Status</th>
-                                        @if ($user->is_admin == '1')
-                                        <th>Action</th>
-                                        @endif
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    @php
-                                        if ($user->is_admin == '1') {
-                                            $payments = App\Models\Payment::join('users', 'users.id', '=', 'payments.user_id')->get(['payments.*', 'users.name', 'users.email']);
-                                        } else {
-                                            $payments = App\Models\Payment::join('users', 'users.id', '=', 'payments.user_id')
-                                                ->where('payments.user_id', $user->id)
-                                                ->get(['payments.*', 'users.name', 'users.email']);
-                                        }
-                                    @endphp
-                                    @foreach ($payments as $payment)
-                                        <tr>
-                                            <td>{{ $payment->created_at }}</td>
-                                            <td>{{ $payment->name }} <br>({{ $payment->email }})</td>
-                                            <td>{{ $payment->taka }}</td>
-                                            <td>{{ $payment->transaction_id }}</td>
-                                            <td>{{ $payment->status }}</td>
-                                            @if ($user->is_admin == '1')
-                                            <td>
-                                                <a href="#">Edit</a>
-                                            </td>
-                                            @endif
-                                        </tr>
-                                    @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+        <!-- DataTales For Payment -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h4 style="float: left" class="m-0 font-weight-bold text-primary">Payments</h4>
+                <a href="{{ URL::to('/add-payment') }}">
+                    <button type="button" class="btn btn-success float-right"><i class="fas fa-plus-circle pt-1"></i>
+                        Add Payment
+                    </button>
+                </a>
             </div>
-            <!-- /.container-fluid -->
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Paid By</th>
+                            <th>Taka</th>
+                            <th>Transaction Id</th>
+                            <th>Status</th>
+                            @if ($user->is_admin == '1')
+                                <th>Action</th>
+                            @endif
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @php
+                            if ($user->is_admin == '1') {
+                                $payments = App\Models\Payment::join('users', 'users.id', '=', 'payments.user_id')->get(['payments.*', 'users.name', 'users.email']);
+                            } else {
+                                $payments = App\Models\Payment::join('users', 'users.id', '=', 'payments.user_id')
+                                    ->where('payments.user_id', $user->id)
+                                    ->get(['payments.*', 'users.name', 'users.email']);
+                            }
+                        @endphp
+                        @foreach ($payments as $payment)
+                            <tr>
+                                <td>{{ $payment->created_at }}</td>
+                                <td>{{ $payment->name }} <br>({{ $payment->email }})</td>
+                                <td>{{ $payment->taka }}</td>
+                                <td>{{ $payment->transaction_id }}</td>
+                                <form>
+                                    <td><select name="paystatus" onchange='if(this.value != 0) { this.form.submit(); }'
+                                                class="form-select center">
+                                            <option>{{ $payment->status }}</option>
+                                            <option style="color: rgb(9, 214, 9);">Approved</option>
+                                            <option style="color: red;">Reject</option>
+                                            <option style="color: blue;">Pending</option>
+                                        </select>
+                                    </td>
+                                    @if ($user->is_admin == '1')
+                                        <td>
+                                            <button type="submit" class="btn btn-primary btn-sm"><i
+                                                    class="fas fa-save"></i></button>
+                                        </td>
+
+                                    @endif
+                                </form>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-        <!-- End of Main Content -->
+    </div>
+    <!-- /.container-fluid -->
+    </div>
+    <!-- End of Main Content -->
 @endsection
